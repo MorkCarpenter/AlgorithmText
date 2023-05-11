4c9b66d8fc2ad3aaa82fe674e69b26855a72e191
MorkCarpenter <f2248472180@outlook.com>
2023-05-11 20:46:30 +0800

    5.11

---
+++ luogu/acwing143.cpp
@@ -0,0 +1,51 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int n;
+int tire[3100005][2], idx = 0;
+int nums[100005];
+void insert(int x) {
+	int p = 0;
+	for (int i = 30; i >= 0; --i) {
+		int u = (x >> i) & 1;
+		if (tire[p][u]) {
+			p = tire[p][u];
+		}
+		else {
+			tire[p][u] = ++idx;
+			p = tire[p][u];
+		}
+	}
+}
+int compare(int x) {
+	int p = 0;
+	int res = 0;
+	for (int i = 30; i >= 0; --i) {
+		int u = (x >> i) & 1;
+		if (tire[p][!u]) {
+			res = res * 2 + 1;
+			p = tire[p][!u];
+		}
+		else {
+			res = res * 2 + 0;
+			p = tire[p][u];
+		}
+	}
+	return res;
+}
+int main() {
+	int ans = 0;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> n;
+	for (int i = 0; i < n; ++i) {
+		cin >> nums[i];
+		insert(nums[i]);
+	}
+	for (int i = 0; i < n; ++i) {
+		ans = max(ans, compare(nums[i]));
+	}
+	cout << ans;
+	return 0;
+}
---
+++ luogu/acwing835.cpp
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+using namespace std;
+
+int trie[100005][32];
+int idx;
+int cnt[100005];
+int n;
+char in[100005], k;
+int main() {
+	scanf("%d", &n);
+	while (n--) {
+		scanf(" %c %s", &k, in);
+		if (k == 'I') {
+			int p = 0;
+			for (int i = 0; in[i]; ++i) {
+				int tmp = in[i] - 'a';
+				if (!trie[p][tmp]) {
+					trie[p][tmp] = ++idx;
+				}
+				p = trie[p][tmp];
+			}
+			++cnt[p];
+		}
+		else {
+			int p = 0;
+			for (int i = 0; in[i]; ++i) {
+				int tmp = in[i] - 'a';
+				if (!trie[p][tmp]) { p = 0; break; }
+				p = trie[p][tmp];
+			}
+			printf("%d\n", cnt[p]);
+		}
+	}
+	return 0;
+}
